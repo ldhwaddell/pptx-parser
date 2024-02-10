657285352ab5d51486c2119fa6be73a14e8fb174
ldhwaddell <ldhwaddell@gmail.com>
2024-02-10 16:42:06 -0400

    add tests for text, image, and audio

--- tests/tests.py
+++ tests/tests.py
@@ -10,9 +10,26 @@ class Test(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        parser_pptx = PptxParser(dir="tests/powerpoints/pptx", recursive=True)
-        parser_image = PptxParser(dir="tests/powerpoints/image", recursive=True)
-        parser_audio = PptxParser(dir="tests/powerpoints/audio", recursive=True)
+        parser_pptx = PptxParser(
+            dir="tests/powerpoints/pptx",
+            save_images_dir="",
+            transcribe_audio=False,
+            recursive=True,
+        )
+
+        parser_image = PptxParser(
+            dir="tests/powerpoints/image",
+            save_images_dir="tests/powerpoints/image",
+            transcribe_audio=False,
+            recursive=True,
+        )
+
+        parser_audio = PptxParser(
+            dir="tests/powerpoints/audio",
+            save_images_dir="",
+            transcribe_audio=True,
+            recursive=True,
+        )
 
         # Redirect output to suppress print
         with io.StringIO() as buf, redirect_stdout(buf):
@@ -36,12 +53,28 @@ class Test(unittest.TestCase):
         self.assertEqual(slide1["slide_text"], "Title Subtitle")
         self.assertEqual(slide2["slide_text"], "Slide Title 1 2")
 
-    # def test_parse_pptx_images(self):
-    #     # Ensure images are properly found
-    #     print(self.parser_image_results)
+    def test_parse_pptx_images(self):
+        # Ensure images are properly found
+        slide2 = self.parser_image_results[0]["slide2"]
+        slide3 = self.parser_image_results[0]["slide3"]
+
+        # Assert images found
+        self.assertEqual(len(slide2["image_paths"]), 1)
+        self.assertEqual(len(slide3["image_paths"]), 1)
+
+        # Assets correct paths found
+        self.assertIn(
+            "tests/powerpoints/image/test_image/slide2_images/image1.png",
+            slide2["image_paths"],
+        )
+        self.assertIn(
+            "tests/powerpoints/image/test_image/slide3_images/image2.jpeg",
+            slide3["image_paths"],
+        )
 
     def test_parse_pptx_audio(self):
         # Ensure audio is properly transcribed
+        print(self.parser_audio_results)
         slide2 = self.parser_audio_results[0]["slide2"]
 
         self.assertEqual(slide2["transcription"], " This is a test.")
